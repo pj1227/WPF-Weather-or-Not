5ec64d427cd6cfbf1d2a77b8b34da8426077a539
pj1227 <joel1227@proton.me>
2026-02-26 14:26:22 -0700

    cleanup unused using statements

--- src/WeatherDashboard/Helpers/TemperatureFormatter.cs
+++ src/WeatherDashboard/Helpers/TemperatureFormatter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WeatherDashboard.Helpers
+﻿namespace WeatherDashboard.Helpers
 {
     public static class TemperatureFormatter
     {
